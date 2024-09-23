8503da82d29d0850ae7b3564d8a5d2af862fd562
Jonathan Rueda <jonaneiro09@gmail.com>
2024-09-23 17:37:51 +0200

    Creación numero random y interfaz

--- app/src/main/java/com/jonathan/endevinaelnum/MainActivity.java
+++ app/src/main/java/com/jonathan/endevinaelnum/MainActivity.java
@@ -1,6 +1,10 @@
 package com.jonathan.endevinaelnum;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,30 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        int numadivinar = (int) (Math.random()*(100-1)+1);
+        final Button button = findViewById(R.id.buttonconfirm);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                @SuppressLint("ResourceType") int num = getText(R.id.textNum).length();
+
+                CharSequence text;
+                int duration;
+                if (numadivinar == num) {
+                    text = "Vamoooos";
+                    duration = Toast.LENGTH_SHORT;
+                } else {
+                    text = ""+numadivinar;
+                    duration = Toast.LENGTH_SHORT;
+
+                }
+                Toast toast = Toast.makeText(MainActivity.this, text,duration);
+                toast.show();
+
+
+            }
         });
     }
 }
